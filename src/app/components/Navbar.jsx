2338a52113342774c126d01ed3566717bbc5cee7
akshat5011 <akshatgarg2k4@gmail.com>
2025-01-17 22:16:49 +0530

    actual fix of error

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 export default function Navbar(params) {
   return (
     <div className="flex justify-center space-x-3 dark:bg-gray-600  bg-amber-100 lg:text-lg  p-4">
+      <Suspense>
       <NavbarItem title="Trending" param="fetchTrending" />
       <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 }
